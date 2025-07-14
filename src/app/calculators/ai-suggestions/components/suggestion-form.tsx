9b82539277d37a291e8fd7c0bfb05162500d951e
root <root@srv896599.hstgr.cloud>
2025-07-14 15:07:43 +0000

    Add SuggestionForm component

---
+++ src/app/calculators/ai-suggestions/components/suggestion-form.tsx
@@ -0,0 +1,8 @@
+export default function SuggestionForm() {
+  return (
+    <form>
+      <input type="text" placeholder="Your suggestion" />
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
